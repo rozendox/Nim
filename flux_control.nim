9693bc4eb64499a91d9610e9dbd3e06e652129ae
Gabriel Rozendo <79455923+rozendox@users.noreply.github.com>
2023-08-09 11:33:01 -0300

    Create flux_control.nim

---
+++ flux_control.nim
@@ -0,0 +1,9 @@
+echo "Digite a sua idade: "
+var idade = readInt()
+
+if idade < 18:
+    echo "Você é menor de idade."
+elif idade == 18:
+    echo "Você tem 18 anos."
+else:
+    echo "Você é maior de idade."
